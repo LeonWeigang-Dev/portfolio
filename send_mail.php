8041dee6f01e250c1116318abc24c6426c9f6fb8
Leon W. <orlando.game@web.de>
2026-09-18 13:35:18 +0200

    Update: Replace contact link in navigation across multiple files and update email subject in send_mail.php

--- send_mail.php
+++ send_mail.php
@@ -51,7 +51,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
 
         // Empfängeradresse (nutzt die oben definierte Mail)
         $recipient = $siteEmail; 
-        $subject = 'Website Contact Form';
+        $subject = 'Portfolio Nachricht';
 
         $mailBody = "
             <strong>Name:</strong> {$safeName}<br>
